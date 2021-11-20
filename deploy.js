5062eecff1b9a2166061984187190c065ac071c2
Vivek Anand <64371138+Dorivika@users.noreply.github.com>
2021-11-20 15:04:01 +0530

    Add files via upload

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+
+const { abi, evm } = require('./compile');
+
+provider = new HDWalletProvider(
+  'crawl animal build village climb hope lens ceiling wasp ski coyote blood',
+  'https://rinkeby.infura.io/v3/2d3bec26c53a46ada5bc0558622729ce'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
